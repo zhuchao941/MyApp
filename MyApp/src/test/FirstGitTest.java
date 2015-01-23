1c650d807511401dd1bc25e221a20a0a76a30160
Zhu <339357762@qq.com>
2015-01-23 14:35:24 +0800

    test

--- MyApp/src/test/FirstGitTest.java
+++ MyApp/src/test/FirstGitTest.java
@@ -4,5 +4,6 @@ public class FirstGitTest {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
 		System.out.println("Hello World again");
+		System.out.println("Hello World 3");
 	}
 }
